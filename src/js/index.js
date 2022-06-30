c805b1c5e3061168c9a320a4d971b3f014c268f0
Joseph Soria <josephsoria6@gmail.com>
2022-06-30 14:37:50 -0500

    Change aria-expanded attribute for nav toggler

--- src/js/index.js
+++ src/js/index.js
@@ -75,10 +75,12 @@ function setToggler() {
     const visibility = navigationContainer.getAttribute("data-visible");
     if (visibility === "false") {
       navigationContainer.setAttribute("data-visible", "true");
+      navigationToggler.setAttribute("aria-expanded", "true");
       document.querySelector('i[class~="closed"]').style.setProperty("display","none");
       document.querySelector('i[class~="open"]').style.setProperty("display","block");
     } else {
       navigationContainer.setAttribute("data-visible", "false");
+      navigationToggler.setAttribute("aria-expanded", "false");
       document.querySelector('i[class~="closed"]').style.setProperty("display","block");
       document.querySelector('i[class~="open"]').style.setProperty("display","none");
     }
