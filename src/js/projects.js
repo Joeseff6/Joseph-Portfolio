9e0880f1fb66c72b8e771905062ef43b22d858be
Joseph Soria <josephsoria6@gmail.com>
2022-07-02 13:12:05 -0500

    Updated projectsObject and added screenshots

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,22 +1,51 @@
 const projectsObject = {
   "react": [
     {
-      name: "project1",
-      description: "My first project",
-      deployedLink: "some link",
-      githubLink: "some other link",
-      screenshot: "image link",
-      techStack: "TML, CSS, Javascript, React"
+      name: "Reactive Calculator",
+      description: "Performing single operations in Javascript is easy. Performing multiple operations using a single line of text becomes challenging. This project demonstrates my ability to write algorithms to solve complex problems.",
+      deployedLink: "https://velvety-treacle-71a2a2.netlify.app",
+      githubLink: "https://github.com/Joeseff6/Reactive-Calculator",
+      screenshot: "./public/images/ReactiveCalculator.PNG",
+      screenshotAlt: "A screenshot of my Reactive Calculator project",
+      techStack: "Javascript, React"
+    },
+    {
+      name: "Task Tracker",
+      description: "A task tracking app built with React. Users are able to store tasks onto their local machine, which persists so the user can keep track, edit, and clear completed tasks.",
+      deployedLink: "https://dreamy-fermat-7e9bea.netlify.app",
+      githubLink: "https://github.com/Joeseff6/Task-Tracker",
+      screenshot: "./public/images/TaskTracker.PNG",
+      screenshotAlt: "A screenshot of my Task Tracker project",
+      techStack: "React, IndexedDB, Dexie"
+    },
+    {
+      name: "MERN-Google-Books",
+      description: "Using the Google Books API, I built one of my first MERN applications featuring user login. A user can create an account and save/delete books of their interest.",
+      deployedLink: "https://whispering-springs-93162.herokuapp.com",
+      githubLink: "https://github.com/Joeseff6/MERN-Google-Books",
+      screenshot: "./public/images/MernGoogleBooks.PNG",
+      screenshotAlt: "A screenshot of my MERN Google Books project",
+      techStack: "MongoDB, Express, React, Node"
     }
   ],
   "sql": [
     {
-      name: "project2",
-      description: "My second project",
-      deployedLink: "some link",
-      githubLink: "some other link",
-      screenshot: "image link",
-      techStack: "TML, CSS, Javascript, SQL"
+      name: "Bloggers Blog",
+      description: "My first solo SQL project, which uses the MVC paradigm. Users can create an account, view/create/edit/delete posts, and comment on other's posts! All persistence is maintained using Heroku's server and SQL database.",
+      deployedLink: "https://bloggers-blog123.herokuapp.com",
+      githubLink: "https://github.com/Joeseff6/BloggersBlog",
+      screenshot: "./public/images/BloggersBlog.PNG",
+      screenshotAlt: "A screenshot of my Bloggers Blog project",
+      techStack: "Javascript, MVC, Handlebars, SQL"
+    },
+    {
+      name: "CLI Employee Tracker",
+      description: "My Employee Tracker app is actually a CLI application that takes a user's input and interacts with a SQL database. The user can determine how to interact with the database depending on their option selected in the CLI.",
+      deployedLink: null,
+      githubLink: "https://github.com/Joeseff6/CLIEmployeeTracker",
+      screenshot: "./public/images/CliEmployeeTracker.PNG",
+      screenshotAlt: "A screenshot of my CLI Employee Tracker project",
+      techStack: "Javascript, SQL, Node, Inquirer"
     }
   ],
   "frontend": [
@@ -42,10 +71,30 @@ const projectsObject = {
       name: "Full Stack Quiz",
       description: "Full Stack Quiz is an early project that I developed during my Bootcamp session. This quiz features local storage usage for storing points, as well as a highscore table!",
       deployedLink: "https://joeseff6.github.io/FullStackCodingQuiz",
-      githubLink: "https://github.com/Joeseff6/HTML-CSS-Tips",
-      screenshot: "./public/images/HtmlCssTips.png",
-      screenshotAlt: "A screenshot of my HTML and CSS Tips project",
-      techStack: "HTML, CSS, Frontend Javascript"
+      githubLink: "https://github.com/Joeseff6/FullStackCodingQuiz",
+      screenshot: "./public/images/FullStackCodingQuiz.PNG",
+      screenshotAlt: "A screenshot of my Full Stack Coding Quiz project",
+      techStack: "HTML, CSS, Frontend Javascript, Local Storage"
+    }
+  ],
+  "cli": [
+    {
+      name: "Team Titans",
+      description: "Team Titans CLI is a powerful application that takes a user's input through a command line, and generates a HMTL file displaying the results. This project uses inquirer for prompts, and classes/OOP for creating new entities.",
+      deployedLink: null,
+      githubLink: "https://github.com/Joeseff6/TeamTitans",
+      screenshot: "./public/images/TeamTitans.PNG",
+      screenshotAlt: "A screenshot of my Team Titans project",
+      techStack: "Javascript, Inquirer.js, OOP, Node"
+    },
+    {
+      name: "Write Run Readme",
+      description: "My first CLI application which takes a user's input and generates a simple README file based on that input. The README file is generated in the folder where the application is ran.",
+      deployedLink: null,
+      githubLink: "https://github.com/Joeseff6/WriteRunReadme",
+      screenshot: "./public/images/WriteRunReadme.PNG",
+      screenshotAlt: "A screenshot of my Write Run Readme project",
+      techStack: "Javascript, Inquirer.js, Node"
     }
   ]
 }
