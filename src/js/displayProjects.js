762d88ea8c02d3cb369680b4b33ae0ae7a25f2b4
Joseph Soria <josephsoria6@gmail.com>
2022-07-02 23:52:09 -0500

    Display projects by pop-in on project selection

--- src/js/displayProjects.js
+++ src/js/displayProjects.js
@@ -1,10 +1,11 @@
 // Import projects to be displayed
 import projectsObject from "./projects.js";
+let popInTimeoutId;
 
 // Main display projects function. Export to main JS file.
 export default function setupProjectButtons() {
   const projectButtons = document.querySelectorAll(".project-type-button");
-  projectButtons.forEach(button => {
+  projectButtons.forEach((button) => {
     button.addEventListener("click", (event) => {
       let projects = [];
       const selectedProjectType = event.target.dataset.type;
@@ -14,7 +15,7 @@ export default function setupProjectButtons() {
         }
       }
       if (!projects.length) {
-        throw ("Project type not found");
+        throw "Project type not found";
       }
       displayProjectButtons(selectedProjectType, projects);
     });
@@ -22,6 +23,45 @@ export default function setupProjectButtons() {
 }
 
 // Functions to actually display project upon project button click
+function displayProjectButtons(projectType, projects) {
+  let buttonContainer = document.querySelector(".project-buttons-container");
+  while (buttonContainer.firstChild) {
+    buttonContainer.removeChild(buttonContainer.firstChild);
+  }
+  projects.forEach((project) => {
+    buttonContainer.removeChild;
+    let button = document.createElement("button");
+    button.innerHTML = project.name;
+    button.setAttribute("class", `project-type-button ${projectType}`);
+    button.addEventListener("click", () => {
+      console.log(project)
+      projectPopIn(project);
+    });
+    buttonContainer.append(button);
+  });
+}
+
+function projectPopIn(project) {
+  let popInContainer = document.querySelector(".popIn-container");
+  let popInContainerClassesArray = Array(...popInContainer.classList);
+  if (
+    !popInContainerClassesArray.includes("pop-in") &&
+    !popInContainerClassesArray.includes("pop-out")
+  ) {
+    displayProject(project);
+    popInContainer.classList.add("pop-in");
+  } else if (popInContainerClassesArray.includes("pop-in")) {
+    clearTimeout(popInTimeoutId);
+    popInContainer.classList.remove("pop-in");
+    popInContainer.classList.add("pop-out");
+    popInTimeoutId = setTimeout(() => {
+      displayProject(project);
+      popInContainer.classList.add("pop-in");
+      popInContainer.classList.remove("pop-out");
+    }, 1000);
+  }
+}
+
 function displayProject(project) {
   let projectHeader = document.querySelector(".project-information-header");
   let projectImage = document.getElementById("project-img");
@@ -39,44 +79,12 @@ function displayProject(project) {
   if (!project.deployedLink) {
     projectDeployedLink.innerText = "Deployment link unavailable";
   } else {
-    projectDeployedLink.setAttribute("href", project.deployedLink)
+    projectDeployedLink.setAttribute("href", project.deployedLink);
     projectDeployedLink.innerText = "Deployed Link";
   }
   projectTechStack.innerText = "Tech Stack: " + project.techStack;
   projectDescription.innerText = project.description;
 }
 
-function displayProjectButtons(projectType, projects) {
-  let buttonContainer = document.querySelector(".project-buttons-container");
-  while (buttonContainer.firstChild) {
-    buttonContainer.removeChild(buttonContainer.firstChild);
-  }
-  projects.forEach(project => {
-    buttonContainer.removeChild
-    let button = document.createElement("button");
-    button.innerHTML = project.name;
-    button.setAttribute("class", `project-type-button ${projectType}`);
-    button.addEventListener("click", () => {
-      projectPopInOut();
-      displayProject(project);
-    });
-    buttonContainer.append(button);
-  })
-}
 
-function projectPopInOut() {
-  let popInOutContainer = document.querySelector(".popInOut-container");
-  let popInOutContainerClassesArray = Array(...popInOutContainer.classList);
 
-  if (!popInOutContainerClassesArray.includes("pop-in") 
-    &&
-    !popInOutContainerClassesArray.includes("pop-out")) {
-      popInOutContainer.classList.add("pop-in");
-    } else if (popInOutContainerClassesArray.includes("pop-in")) {
-      popInOutContainer.classList.remove("pop-in");
-      popInOutContainer.classList.add("pop-out");
-    } else if (popInOutContainerClassesArray.includes("pop-out")) {
-      popInOutContainer.classList.remove("pop-out");
-      popInOutContainer.classList.add("pop-in");
-    }
-}
